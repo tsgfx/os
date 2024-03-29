2c2edf5761ee3cad97cbfdeeb89e2dc2b8fed01a
tsgfx <grcsxb269@163.com>
2024-03-29 17:02:08 +0800

    add lab3 dir

---
+++ lab3/test_program.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    int x, i;
+
+    printf("Input a initial value for i: ");
+    scanf("%d", &i);
+    while((x=fork())==-1);
+    if(x==0)		/* child run */
+    {
+        printf("When child runs, i=%d\n", i);
+        printf("Input a value in child: ");
+        scanf("%d", &i);
+        printf("i=%d\n", i);
+    }
+    else		/* parent run */
+    {
+        wait(NULL);
+        printf("After child runs, in parent, i=%d\n", i);
+    }
+}
